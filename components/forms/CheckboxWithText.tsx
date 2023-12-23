0767d905ddda1a711089e8c6814b1734b825f9e1
Sergio Nezhigay <abc1971h@gmail.com>
2023-12-23 15:40:46 +0200

    update checkbox

--- components/forms/CheckboxWithText.tsx
+++ components/forms/CheckboxWithText.tsx
@@ -29,14 +29,12 @@ const CheckboxWithText: React.FC<ICheckbox> = ({ control, name, register }) => {
                 />
               </div>
             </FormControl>
+            <FormLabel className="text-[12px] font-extralight leading-[22px] ">
+              I confirm my consent to the processing of personal data.
+            </FormLabel>
             <FormDescription className="sr-only">
               Agree to the terms
             </FormDescription>
-            <div className="space-y-1 leading-none md:max-w-[192px]">
-              <FormLabel className="text-[12px] font-extralight leading-[22px] ">
-                I confirm my consent to the processing of personal data.
-              </FormLabel>
-            </div>
           </FormItem>
           <FormMessage
             className={`absolute bottom-[5px] text-[12px] font-extralight tracking-[0.20em] text-[#FF5757] max-md:max-w-[200px] md:bottom-[-25px] `}
