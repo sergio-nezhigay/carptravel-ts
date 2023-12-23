84d04f3bc415a48c42f9df3e8fa3bd84a9537401
Sergio Nezhigay <abc1971h@gmail.com>
2023-12-23 16:10:49 +0200

    test1

--- sections/home/MobileNav.tsx
+++ sections/home/MobileNav.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link as ScrollLink } from "react-scroll";
-import { Button } from "@/components/ui/button";
+
 import {
   Sheet,
   SheetClose,
   SheetContent,
+  SheetDescription,
   SheetHeader,
+  SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
 import {
@@ -17,39 +19,49 @@ import { MENU_ITEMS } from "@/constants/home";
 const MobileNav: React.FC = () => {
   return (
     <Sheet>
-      <SheetTrigger asChild>
-        <Button
-          variant="outline"
-          className="text-[14px] leading-[17px] tracking-widest text-white md:hidden"
-        >
-          MENU
-        </Button>
-      </SheetTrigger>
-      <SheetContent className="flex-center w-full border-none bg-[#010A05BF] px-5 py-[43px]">
+      <SheetTrigger>Open</SheetTrigger>
+      <SheetContent>
         <SheetHeader>
-          <SheetTrigger className="absolute right-5 top-[43px] z-50 ml-auto text-[14px] leading-[17px] tracking-widest text-white">
-            CLOSE
-          </SheetTrigger>
+          <SheetTitle>Are you sure absolutely sure?</SheetTitle>
+          <SheetDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </SheetDescription>
         </SheetHeader>
-        <NavigationMenuList className="flex flex-col gap-12">
-          {MENU_ITEMS.map((menuItem) => (
-            <NavigationMenuItem key={menuItem}>
-              <SheetClose asChild>
-                <ScrollLink
-                  href={`#${menuItem}`}
-                  to={`${menuItem}`}
-                  smooth={true}
-                  duration={500}
-                  className="underline-on-hover text-[18px] capitalize leading-[22px] tracking-widest text-white"
-                >
-                  {menuItem}
-                </ScrollLink>
-              </SheetClose>
-            </NavigationMenuItem>
-          ))}
-        </NavigationMenuList>
       </SheetContent>
     </Sheet>
+    // <Sheet>
+    //   <SheetTrigger
+    //     asChild
+    //     className="text-[14px] leading-[17px] tracking-widest text-white md:hidden"
+    //   >
+    //     MENU
+    //   </SheetTrigger>
+    //   <SheetContent className="flex-center w-full border-none bg-[#010A05BF] px-5 py-[43px]">
+    //     <SheetHeader>
+    //       <SheetTrigger className="absolute right-5 top-[43px] z-50 ml-auto text-[14px] leading-[17px] tracking-widest text-white">
+    //         CLOSE
+    //       </SheetTrigger>
+    //     </SheetHeader>
+    //     <NavigationMenuList className="flex flex-col gap-12">
+    //       {MENU_ITEMS.map((menuItem) => (
+    //         <NavigationMenuItem key={menuItem}>
+    //           <SheetClose asChild>
+    //             <ScrollLink
+    //               href={`#${menuItem}`}
+    //               to={`${menuItem}`}
+    //               smooth={true}
+    //               duration={500}
+    //               className="underline-on-hover text-[18px] capitalize leading-[22px] tracking-widest text-white"
+    //             >
+    //               {menuItem}
+    //             </ScrollLink>
+    //           </SheetClose>
+    //         </NavigationMenuItem>
+    //       ))}
+    //     </NavigationMenuList>
+    //   </SheetContent>
+    // </Sheet>
   );
 };
 
