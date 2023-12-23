2eaadb1deb14dc027aa2beaf5f5abf0819ff49a6
Sergio Nezhigay <abc1971h@gmail.com>
2023-12-23 14:06:59 +0200

    test validation

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 
-import About from "@/sections/about/About";
-import Gallery from "@/sections/gallery/Gallery";
-import Services from "@/sections/services/Services";
-import Carrer from "@/sections/career/Career";
-import Home from "@/sections/home/Home";
+// import About from "@/sections/about/About";
+// import Gallery from "@/sections/gallery/Gallery";
+// import Services from "@/sections/services/Services";
+// import Carrer from "@/sections/career/Career";
+// import Home from "@/sections/home/Home";
 import Contacts from "@/sections/contacts/Contacts";
 
 const Page: React.FC = () => {
   return (
     <>
-      <Home />
+      {/* <Home />
       <About />
       <Services />
       <Carrer />
-      <Gallery />
+      <Gallery /> */}
       <Contacts />
     </>
   );
