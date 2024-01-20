489112ae35f2e98c8c3a6c5e2809eb1be7656818
Sergio Nezhigay <abc1971h@gmail.com>
2024-01-20 11:59:33 +0200

    test env VERCEL_URL5

--- constants/layout.ts
+++ constants/layout.ts
@@ -1,7 +1,9 @@
 // export const BASE_URL = "https://carptravel-ts.vercel.app/";
 // export const { VERCEL_URL = "http://localhost:3000" } = process.env;
 
-export const TEST_URL = process.env.NEXT_PUBLIC_VERCEL_URL;
+// export const TEST_URL = ${process.env.NEXT_PUBLIC_VERCEL_URL}
+export const TEST_URL = "http://localhost:3000";
+console.log(process.env);
 
 export const TITLE = "CarpTravel";
 export const DESCRIPTION =
