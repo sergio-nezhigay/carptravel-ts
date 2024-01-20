a0c62d1a3c8d538a2f25dd42b76d1c4172f493e1
Sergio Nezhigay <abc1971h@gmail.com>
2024-01-20 11:16:24 +0200

    test env

--- constants/layout.ts
+++ constants/layout.ts
@@ -1,4 +1,7 @@
-export const BASE_URL = "https://carptravel-ts.vercel.app/";
+// export const BASE_URL = "https://carptravel-ts.vercel.app/";
+export const { NEXT_PUBLIC_MAIN_SITE_URL = "http://localhost:3000" } =
+  process.env;
+
 export const TITLE = "CarpTravel";
 export const DESCRIPTION =
   "We offer you unforgettable trips to the most beautiful parts of the Carpathians. Enjoy stunning views, exciting expeditions, and the best service!";
